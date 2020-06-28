837f55b0c07083ec24b1764b783fa360edb9818b
joao Paulo <joaopaulozangrande@gmail.com>
2020-06-28 17:34:15 -0300

    i create a rout to open files

--- app/routes/index.js
+++ app/routes/index.js
@@ -15,6 +15,39 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/file', (req,res)=>{
+
+  let path = './' + req.query.path;
+
+  if (fs.existsSync(path)){
+
+    fs.readFile(path, (err,data)=>{
+
+      if (err){
+
+        console.error(err);
+        res.status(400).json({
+          error: err
+        });
+
+      } else {
+
+        res.status(200).end(data);
+
+      }
+
+    });
+
+  } else {
+
+    res.status(404).json({
+      error: 'File not found.'
+    })
+
+  }
+
+});
+
 router.delete('/file', (req,res)=>{
 
   let form = new formidable.IncomingForm({
@@ -41,6 +74,12 @@ router.delete('/file', (req,res)=>{
         }
 
       });
+
+    } else {
+
+      res.status(404).json({
+        error: 'File not found.'
+      })
 
     }
 
